a81cd4de63418bf298132954961737130e745406
Daniel Badinszky <badinszky97@gmail.com>
2024-04-12 09:46:00 +0200

    Térkép szélessége az ablakmérethez igazitva

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -2,5 +2,5 @@
 <p>Ügyvezető: <strong>Valaki Az</strong></p>
 <p>E-mail: <strong>valaki.az@minihonlap.hu</strong></p>
 <p>Üzletünk: <strong>A Láncídon.</strong></p>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2695.533712689421!2d19.041124476739657!3d47.49899639540139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dc3fb79a1a29%3A0x1e40fed785d0c9a4!2zU3rDqWNoZW55aSBMw6FuY2jDrWQ!5e0!3m2!1shu!2shu!4v1712907503307!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br>
+<iframe id=terkep src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2695.533712689421!2d19.041124476739657!3d47.49899639540139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dc3fb79a1a29%3A0x1e40fed785d0c9a4!2zU3rDqWNoZW55aSBMw6FuY2jDrWQ!5e0!3m2!1shu!2shu!4v1712907503307!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br>
 <a target="_blank" href="https://maps.app.goo.gl/hgweQbJVVUnPEfWq9">Nagyobb térkép</a>
